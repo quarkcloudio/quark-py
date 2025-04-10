956b6cc51cbb717ebbc51e575336bc6ae8e45915
tangtanglove <dai_hang_love@126.com>
2025-04-10 18:26:21 +0800

    chore: wip

--- quark.py
+++ quark.py
@@ -1,1 +1,22 @@
 #coding: utf-8
+from flask import Flask
+from api.detect import detect_bp
+from api.ocr import ocr_bp
+
+# 创建一个app
+app = Flask(__name__)
+
+# 设置debug模式
+app.config["DEBUG"] = True
+
+# 注册蓝图
+app.register_blueprint(detect_bp)
+app.register_blueprint(ocr_bp)
+
+# 创建路由
+@app.route('/')
+def index() -> str:
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
