f696848065cf63099e90416f6ab0191bed43cfa8
tangtanglove <dai_hang_love@126.com>
2025-04-11 16:29:04 +0800

    chore: wip

--- src/quark/quark.py
+++ src/quark/quark.py
@@ -1,8 +1,9 @@
 #coding: utf-8
 from typing import Any
 from flask import Flask
-from .template.controller.resource import resource_bp
+from quark.template.controller.resource import resource_bp
 
 class Quark(Flask):
     def __init__(self, __name__):
         super().__init__(__name__)
+        self.register_blueprint(resource_bp)
--- src/quark/template/controller/resource.py
+++ src/quark/template/controller/resource.py
@@ -4,10 +4,6 @@ from flask import Blueprint, request, jsonify
 resource_bp = Blueprint('resource', __name__)
 
 # 定义路由
-@resource_bp.route('/api/admin/:resource/index', methods=['GET'])
-def index():
-    url = request.args.get('url', '')
-    if url == '':
-        return 'url is empty'
-    
-    return "resource"
+@resource_bp.route('/api/admin/<resource>/index', methods=['GET'])
+def index(resource):
+    return resource
