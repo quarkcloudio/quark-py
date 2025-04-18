cc9ca1b3657f55a44c8c97d54a94df156d19ebc3
tangtanglove <dai_hang_love@126.com>
2025-04-18 13:54:17 +0800

    chore: wip

--- src/quark/controller/login.py
+++ src/quark/controller/login.py
@@ -7,4 +7,4 @@ login_bp = Blueprint('login', __name__)
 # 定义路由
 @login_bp.route('/api/admin/login/<resource>/index', methods=['GET'])
 def index(resource):
-    return login_render.index_render(resource)
+    return login_render.index_render()
