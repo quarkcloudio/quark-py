f6421336a4e9df3547dad335672da73c65e70369
tangtanglove <dai_hang_love@126.com>
2025-04-22 14:37:05 +0800

    chore: wip

--- src/quark/template/component/form/fields/when.py
+++ src/quark/template/component/form/fields/when.py
@@ -3,10 +3,10 @@ from typing import Any, List, Optional
 from ...component.element import Element
 
 class Item(Element):
-    component: str = ""          # 组件名称
-    condition: str = ""           # 条件：js表达式语句
-    condition_name: str = ""      # 需要对比的字段名称
-    condition_operator: str = ""  # 操作符，= <>
+    component: str = "whenItem"     # 组件名称
+    condition: str = None           # 条件：js表达式语句
+    condition_name: str = None      # 需要对比的字段名称
+    condition_operator: str = None  # 操作符，= <>
     option: Optional[Any] = None # 条件符合的属性值
     body: Optional[Any] = None    # 内容
 
