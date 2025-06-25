d143b320b23500b7dccc7f196e8543722f31174b
tangtanglove <dai_hang_love@126.com>
2025-06-25 17:57:19 +0800

    chore: wip

--- src/quark/app/actions/edit_link.py
+++ src/quark/app/actions/edit_link.py
@@ -1,22 +1,16 @@
-from typing import Any, Optional
-from app.core.context import Context
-from app.template.admin.resource.actions import Link
+from typing import Optional
+from quark import Request
+from quark.template.action import Link
 
 
-class EditLinkAction(Link):
+class EditLink(Link):
     def __init__(self, name: Optional[str] = None):
-        super().__init__()
         self.name = name or "编辑"
         self.type = "link"
         self.size = "small"
         self.set_only_on_index_table_row(True)
 
-    def init(self, ctx: Context) -> Any:
-        # 可添加初始化逻辑，这里直接返回自己
-        return self
-
-    def get_href(self, ctx: Context) -> str:
+    def get_href(self, request: Request) -> str:
         # 替换路径中的 /index 为 /edit&id=${id}
-        path = ctx.path
-        href = path.replace("/index", "/edit&id=${id}")
+        href = request.url.path.replace("/index", "/edit&id=${id}")
         return f"#/layout/index?api={href}"
