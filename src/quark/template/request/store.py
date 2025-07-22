2e2ce10cf9ccce9e2cec761128f8a75400cf39b1
tangtanglove <dai_hang_love@126.com>
2025-07-22 15:12:42 +0800

    chore: wip

--- src/quark/template/request/store.py
+++ src/quark/template/request/store.py
@@ -1,37 +1,53 @@
 import json
 from pydantic import ValidationError
 from typing import Any, Dict
-from quark.context import Context  # 假设你封装了类似 quark.Context 的 Python 类
-from quark.template.admin.resource.types import Resourcer  # 自定义接口定义
-from tortoise import Tortoise
-from tortoise.exceptions import DoesNotExist
+from fastapi import Request
+from tortoise.models import Model
+from ...component.message.message import Message
 
 
 class StoreRequest:
 
-    async def handle(self, ctx: Context, data: Dict[str, Any]) -> Any:
-        # 模版实例
-        template: Resourcer = ctx.template
+    # 请求对象
+    request: Request = None
 
-        # 模型结构体
-        model_instance = template.get_model()  # 返回一个模型类
-        data_instance = template.get_model()()  # 创建一个新实例
+    # 资源对象
+    resource: Any = None
 
+    # 模型对象
+    model: Model = None
+
+    # 查询对象
+    query: Model = None
+
+    def __init__(
+        self,
+        request: Request,
+        resource: Any,
+        model: Model,
+        query: Model,
+    ):
+        self.request = request
+        self.resource = resource
+        self.model = model
+        self.query = query
+
+    async def handle(self, request: Request, data: Dict[str, Any]) -> Any:
         # 验证数据合法性
         try:
-            await template.validator_for_creation(ctx, data)
+            await self.resource.validator_for_creation(request, data)
         except ValidationError as e:
-            return ctx.json_error(str(e))
+            return Message.error(str(e))
 
         # 保存前回调
         try:
-            data = await template.before_saving(ctx, data)
+            data = await self.resource.before_saving(request, data)
         except Exception as e:
-            return ctx.json_error(str(e))
+            return Message.error(str(e))
 
         # 重组数据
         new_data = {}
-        model_fields = data_instance.__class__.__annotations__.keys()
+        model_fields = self.model.__class__.__annotations__.keys()
 
         for k, v in data.items():
             nv = v
@@ -47,26 +63,25 @@ class StoreRequest:
 
         # 数据赋值
         for field, value in new_data.items():
-            setattr(data_instance, field, value)
+            setattr(self.model, field, value)
 
         # 保存数据
-        await data_instance.save()
+        await self.model.save()
 
         # 因为 tortoise 不会更新零值，我们再使用 update 对部分字段重新更新
-        if not hasattr(data_instance, "id"):
-            return ctx.json_error("参数错误")
+        if not hasattr(self.model, "id"):
+            return Message.error("参数错误")
 
-        id = data_instance.id
-        await data_instance.__class__.filter(id=id).update(**new_data)
+        id = self.model.id
+        await self.model.__class__.filter(id=id).update(**new_data)
 
         # 保存后回调
         try:
-            await template.after_saved(ctx, id, data, data_instance)
+            await self.resource.after_saved(request, id, data, self.model)
         except Exception as e:
-            return ctx.json_error(str(e))
+            return Message.error(str(e))
 
-        return await template.after_saved_redirect_to(ctx, id, data, None)
+        return await self.resource.after_saved_redirect_to(request, id, data, None)
 
     def to_pascal_case(self, s: str) -> str:
-        # 替代 stringy.New(k).PascalCase("?", "").Get()
         return "".join(word.capitalize() for word in s.split("_"))
