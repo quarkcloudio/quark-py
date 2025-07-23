ef82c915c1d1837a34861b1ce51067531cf982c2
tangtanglove <dai_hang_love@126.com>
2025-07-23 17:21:52 +0800

    chore: wip

--- src/quark/template/request/update.py
+++ src/quark/template/request/update.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict
+from typing import Any
 from fastapi import Request
 from tortoise.models import Model, QuerySet
 from ..performs_queries import PerformsQueries
