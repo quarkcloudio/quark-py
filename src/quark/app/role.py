2bb737a709ca2cb4d053780a458a9171eca0f06a
tangtanglove <dai_hang_love@126.com>
2025-07-24 16:46:59 +0800

    chore: wip

---
+++ src/quark/app/role.py
@@ -0,0 +1,52 @@
+from pydantic import model_validator
+from typing import List, Dict
+from quark import models, Request, Resource
+from quark.app import searches, actions
+from quark.component.form import field
+
+
+class Role(Resource):
+    """
+    角色管理
+    """
+
+    @model_validator(mode="after")
+    def init(self):
+
+        # 页面标题
+        self.title = "角色"
+
+        # 模型
+        self.model = models.Role
+
+        return self
+
+    async def fields(self, request: Request) -> List[Dict]:
+        """字段定义"""
+        return [
+            field.id("id", "ID"),
+            field.text("name", "名称"),
+            field.text("path", "路径"),
+            field.select("method", "方法"),
+            field.textarea("remark", "备注"),
+        ]
+
+    async def searches(self, request: Request) -> List[Dict]:
+        """搜索项定义"""
+        return [
+            searches.Input("name", "名称"),
+            searches.Input("path", "路径"),
+        ]
+
+    async def actions(self, request: Request) -> List[Dict]:
+        """行为定义"""
+        return [
+            actions.CreateLink(self.title),
+            actions.BatchDelete(),
+            actions.EditLink(),
+            actions.Delete(),
+            actions.FormSubmit(),
+            actions.FormReset(),
+            actions.FormBack(),
+            actions.FormExtraBack(),
+        ]
