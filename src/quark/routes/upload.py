38d923773cc0560bb265f1bff57a927b5d484285
tangtanglove <dai_hang_love@126.com>
2025-08-02 14:59:42 +0800

    chore: wip

--- src/quark/routes/upload.py
+++ src/quark/routes/upload.py
@@ -4,6 +4,38 @@ from .. import loader
 router = APIRouter(prefix="/api/admin", tags=["文件上传"])
 
 
+@router.get("/upload/{resource}/getList")
+async def get_list(request: Request, resource: str):
+    res = await loader.load_resource_object(request, resource, "Upload")
+    return JSONResponse(
+        content=jsonable_encoder(await res.get_list(request), exclude_none=True)
+    )
+
+
+@router.get("/upload/{resource}/delete")
+async def delete_get(request: Request, resource: str):
+    res = await loader.load_resource_object(request, resource, "Upload")
+    return JSONResponse(
+        content=jsonable_encoder(await res.delete(request), exclude_none=True)
+    )
+
+
+@router.post("/upload/{resource}/delete")
+async def delete_post(request: Request, resource: str):
+    res = await loader.load_resource_object(request, resource, "Upload")
+    return JSONResponse(
+        content=jsonable_encoder(await res.delete(request), exclude_none=True)
+    )
+
+
+@router.post("/upload/{resource}/crop")
+async def crop(request: Request, resource: str):
+    res = await loader.load_resource_object(request, resource, "Upload")
+    return JSONResponse(
+        content=jsonable_encoder(await res.crop(request), exclude_none=True)
+    )
+
+
 @router.post("/upload/{resource}/handle")
 async def handle(request: Request, resource: str):
     res = await loader.load_resource_object(request, resource, "Upload")
