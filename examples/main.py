754154f8d6da87e7602faa921f317e39bb5c6b32
tangtanglove <dai_hang_love@126.com>
2025-08-12 12:46:45 +0800

    chore: wip

---
+++ examples/main.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from quark import Quark, Response
+
+# 创建对象
+app = Quark()
+
+# 配置数据库
+app.config["DB_URL"] = "sqlite://data.db"
+
+
+# 创建路由
+@app.get("/")
+def index() -> str:
+    return Response(content="Hello World!", media_type="text/html")
+
+
+if __name__ == "__main__":
+
+    # 启动应用
+    app.run("quark.__main__:app", host="0.0.0.0", port=4000, reload=True)
