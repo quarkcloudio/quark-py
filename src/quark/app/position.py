c4fb30f581f03303aed412d652e889637ca1c78f
tangtanglove <dai_hang_love@126.com>
2025-08-16 17:11:25 +0800

    chore: wip

--- src/quark/app/position.py
+++ src/quark/app/position.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 from quark import Request, Resource, models
 from quark.app import actions, searches
 from quark.component.form import field
+from quark.component.form.rule import Rule
 
 
 class Position(Resource):
@@ -18,34 +19,45 @@ class Position(Resource):
         # 模型
         self.model = models.Position
 
+        # 排序字段
+        self.index_query_order = "id, sort"
+
         return self
 
     async def fields(self, request: Request) -> List[Dict]:
         """字段定义"""
         return [
             field.id("id", "ID"),
-            field.text("name", "名称"),
-            field.text("path", "路径"),
-            field.select("method", "方法"),
+            field.text("name", "名称").set_rules(
+                [
+                    Rule.required("职位名称必须填写"),
+                    Rule.min(2, "职位名称不能少于2个字符"),
+                    Rule.max(100, "职位名称不能超过100个字符"),
+                ]
+            ),
+            field.number("sort", "排序").set_editable(True).set_default_value(0),
             field.textarea("remark", "备注"),
+            field.switch("status", "状态")
+            .set_editable(True)
+            .set_true_value("正常")
+            .set_false_value("禁用")
+            .set_default_value(True),
         ]
 
     async def searches(self, request: Request) -> List[Dict]:
         """搜索项定义"""
         return [
             searches.Input("name", "名称"),
-            searches.Input("path", "路径"),
+            searches.Status(),
         ]
 
     async def actions(self, request: Request) -> List[Dict]:
         """行为定义"""
         return [
-            actions.CreateLink(self.title),
+            actions.CreateModal(self),
             actions.BatchDelete(),
-            actions.EditLink(),
+            actions.BatchDisable(),
+            actions.BatchEnable(),
+            actions.EditModal(self),
             actions.Delete(),
-            actions.FormSubmit(),
-            actions.FormReset(),
-            actions.FormBack(),
-            actions.FormExtraBack(),
         ]
