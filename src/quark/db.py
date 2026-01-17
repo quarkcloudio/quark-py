2c7fa3c95d9d15981ad51fe8c52fbc714eaf9960
tangtanglove <dai_hang_love@126.com>
2026-01-17 11:53:24 +0800

    fix: 过滤警告

--- src/quark/db.py
+++ src/quark/db.py
@@ -1,4 +1,6 @@
-from typing import Iterable, Any
+import warnings
+from typing import Any, Iterable
+
 from tortoise import Tortoise
 
 
@@ -14,5 +16,10 @@ async def init(
         config_file=config_file,
         db_url=db_url,
         modules=modules,
+    )
+
+    # 忽略表已存在的警告
+    warnings.filterwarnings(
+        "ignore", category=Warning, message=".*Table.*already exists.*"
     )
     await Tortoise.generate_schemas()
